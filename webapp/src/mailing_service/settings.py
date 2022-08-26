1d9bb74e7ca5d48a62f6c41966fc0598fc75cab7
Artem Kirillov <74914141+airyou-code@users.noreply.github.com>
2022-08-26 13:55:51 +0200

    Update settings.py

--- webapp/src/mailing_service/settings.py
+++ webapp/src/mailing_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!)8++meqe&m#^qi@j1y2^o!r+7=_(92$^4cqm6%d#qbx^5pdn!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
